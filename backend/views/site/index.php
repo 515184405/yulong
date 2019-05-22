f7bc67e83a74d5ad74f802a545615d23d48ed7da
fengjianji2 <515184405@qq.com>
2019-05-22 23:58:42 +0800

    a

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,1 +1,12 @@
-11111111111
+<div class="layui-card">
+    <div class="layui-card-body">
+        <h1>您已成功登录聚友团队后台管理</h1>
+    </div>
+</div>
+<script>
+    layui.config({
+        base: './asset/' //静态资源所在路径
+    }).extend({
+        index: 'lib/index' //主入口模块
+    }).use('index');
+</script>
