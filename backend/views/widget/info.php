6dbe49afb99866ea272de8fe1dcde5e414e97150
fengjianji <515184405@qq.com>
2019-06-26 17:26:03 +0800

    a

--- backend/views/widget/info.php
+++ backend/views/widget/info.php
@@ -56,14 +56,6 @@ use kucha\ueditor\UEditor;
                     </div>
                 </div>
             </div>
-            <div id="wx_link" class="layui-form-item ">
-                <label class="layui-form-label">作品压缩包</label>
-                <div class="layui-input-block">
-                    <button type="button" class="layui-btn layui-btn-primary js_upload_image" id="upload-zip">上传文件</button><span class="theme-red ml10">格式为：zip|rar</span>
-                    <input type="hidden" value="<?=isset($data['widget']['download']) ? $data['widget']['download'] : ''?>" class="js_website" name="website">
-                    <a href="<?=isset($data['widget']['download']) ? Yii::$app->params['frontend_url'].$data['widget']['download'] : ''?>" id="zip-upload-demoText"><?=isset($data['widget']['download']) ? Yii::$app->params['frontend_url'].$data['widget']['download'] : ''?></a>
-                </div>
-            </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">入口文件名</label>
                 <div class="layui-input-block">
@@ -93,7 +85,16 @@ use kucha\ueditor\UEditor;
                     ]);?>
                 </div>
             </div>
-
+            <div class="layui-form-item layui-form-text">
+                <label class="layui-form-label">审核状态</label>
+                <div class="layui-input-block">
+                    <select id="select-status" lay-filter="select-status" name="status" lay-verify="required">
+                        <option  value="0">审核中</option>
+                        <option  value="2">未通过</option>
+                        <option  value="1">已完成</option>
+                    </select>
+                </div>
+            </div>
             <div class="layui-form-item">
                 <label class="layui-form-label">下载金币数</label>
                 <div class="layui-input-block">
@@ -130,10 +131,9 @@ use kucha\ueditor\UEditor;
     }).extend({
         index: 'lib/index', //主入口模块
         select2:'../lib/select2/js/select2.min'
-    }).use(['index', 'form','upload','select2'], function(){
+    }).use(['index', 'form','select2'], function(){
 
         var $ = layui.$,
-            upload = layui.upload,
             form = layui.form;
 
         //设置百度编辑器代码块样式
@@ -186,36 +186,6 @@ use kucha\ueditor\UEditor;
                     });
                 }
             })
-
-        //压缩包上传
-        //选完文件后不自动上传
-        upload.render({
-            elem: '#upload-zip'
-            ,url: '/widget/upload-file'
-            ,accept: 'file' //普通文件
-            ,exts: 'zip|rar' //只允许上传压缩文件
-            ,auto: false
-            ,bindAction: '#upload-file-submit'
-            ,done: function(res){
-
-                layer.closeAll();
-                if(res.code == 100000){
-                    $("#zip-upload-demoText").attr('href',frontend_url+res.download).html(res.name);
-                    $('.js_website').val(res.download);
-
-                    layer.confirm(res.message+'是否返回列表？', {
-                        btn: ['确定','取消'] //按钮
-                    }, function(){
-                        window.history.go(-1);
-                    }, function(){
-                        layer.closeAll();
-                        window.location.reload();
-                    });
-                }
-
-            }
-        });
-
 
         // select2初始化
 
@@ -279,20 +249,6 @@ use kucha\ueditor\UEditor;
                 success: function(res) {
                     layer.closeAll();
                     _this.disabled=false;
-                    if(zipFile) {
-                        $('#upload-file-submit').click();
-                        layer.msg('正在上传图片...', {
-                            icon: 16,
-                            time: 0,
-                            shade: [0.1, '#000']
-                        })
-                    }else{
-                        layer.msg(res.message,{icon:1,time:1500},function(){
-                            window.history.go(-1);
-                        })
-
-
-                    }
                 },
                 error: function(){
                     layer.closeAll();
