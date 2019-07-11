da9281f8d1961049950c2b2d0478f6ca392de2db
fengjianji <515184405@qq.com>
2019-07-11 15:24:20 +0800

    a

--- frontend/views/map/index.php
+++ frontend/views/map/index.php
@@ -1,6 +1,6 @@
 <?php $this->title="网站地图"; ?>
-<h1><a href="www.yu313.cn">313组件库</a></h1>
-<h2><a href="www.yu313.cn/unit">组件</a></h2>
+<h1><a href="/">313组件库</a></h1>
+<h2><a href="/unit">组件</a></h2>
 <?php foreach ($data as $val) { ?>
-    <h5><a href="www.yu313.cn/unit/item/<?=$val['id']?>"><?=$val['title']?></a></h5>
+    <h5><a href="/unit/item/<?=$val['id']?>"><?=$val['title']?></a></h5>
 <?php } ?>
