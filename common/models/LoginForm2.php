ca68931bc61eb89c7b994f26d0ebff1efe5dd1b0
fengjianji <515184405@qq.com>
2019-11-26 14:24:47 +0800

    a

--- common/models/LoginForm2.php
+++ common/models/LoginForm2.php
@@ -65,8 +65,8 @@ class LoginForm2 extends Model
     public function login()
     {
         if ($this->validate()) {
-            //一天后自动过期
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 : 0);
+            //30天后自动过期
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
         }
 
         return false;
