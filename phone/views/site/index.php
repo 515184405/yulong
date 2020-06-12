57f900f80293148dd1f86dcb93ec7e676042c04d
fengjianji <515184405@qq.com>
2020-06-13 04:19:45 +0800

    a

--- phone/views/site/index.php
+++ phone/views/site/index.php
@@ -15,6 +15,17 @@ use common\models\Zixun;
         base: '/asset/' //静态资源所在路径
     }).extend({
         index: 'lib/index' //主入口模块
-    }).use(['index']);
+    }).use(['index'],function(){
+        //询问框
+        if(localStorage.getItem('warn_tips')) return false;
+        layer.confirm('<span style="color:#f00;">在使用本站期间，上传的图片与内容请勿侵权他人<span>', {
+            btn: ['确定'], //按钮
+            title:'警告',
+            closeBtn : false,
+        }, function(){
+            localStorage.setItem('warn_tips', true);
+            layer.closeAll();
+        });
+    });
 </script>
 
