b3ed4d56f117a62c77842651e6b3c78aac222e48
fengjianji <515184405@qq.com>
2020-07-06 00:02:14 +0800

    a

--- phone/controllers/ApiController.php
+++ phone/controllers/ApiController.php
@@ -74,17 +74,36 @@ class ApiController extends Controller
         return $this->convertJson('200','查询成功',$contact,0);
     }
 
+    /* 获取筛选条件数据 */
+    public function actionGetFilter(){
+        //读数据
+        $params = \Yii::$app->request->get();
+        $nav = TelNav::find()->orderBy(['sort'=>SORT_DESC])->asArray()->all();
+        $price = array(
+            array("id"=>'DESC',"title"=> "价格由高到低"),
+            array("id"=>'ACE',"title"=> "价格由低到高"),
+            array("id"=>'0-99',"title"=>"0~99"),
+            array("id"=>'100-499',"title"=>"100~499"),
+            array("id"=>'500-999',"title"=>"500~999"),
+            array("id"=>'1000-4999',"title"=>"1000~4999"),
+            array("id"=>'5000-9999',"title"=>"5000~9999"),
+            array("id"=>'10000-49999',"title"=>"10000~49999"),
+            array("id"=>'50000-',"title"=>"50000以上"),
+        );
+        $data = [
+            'online' => $this->getOnline(),
+            'nav' => $nav,
+            'price' => $price,
+        ];
+        return $this->convertJson('200','查询成功',$data, 0);
+    }
+
     /* 获取号码列表 */
     public function actionSelectTel(){
         //读数据
         $params = \Yii::$app->request->get();
         $numberList = TelList::search($params);
-        $nav = TelNav::find()->orderBy(['sort'=>SORT_DESC])->asArray()->all();
-        $data = [
-            'numberList' => array('data'=>$numberList['data'],'count'=>$numberList['count']),
-            'online' => $this->getOnline(),
-            'nav' => $nav,
-        ];
+        $data = array('data'=>$numberList['data'],'count'=>$numberList['count']);
         return $this->convertJson('200','查询成功',$data, 0);
     }
 }
