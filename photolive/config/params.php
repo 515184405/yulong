0811235a447a4eb22f9896de268a6fa31c6f98c5
fengjianji <515184405@qq.com>
2020-09-12 23:38:01 +0800

    QEWRUIOPA

--- photolive/config/params.php
+++ photolive/config/params.php
@@ -1,6 +1,15 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    // TOKEN验证参数
+    'auth'	=>	[
+        //KEY值(MD5加密/可自定义)
+        'key'	=>	'f1e358c80759c68a37394c18755ba7de',
+        //密钥(可自定义)
+        'secret' =>	'photolive19930313',
+        //有效期,单位为s。此处设置为12小时(可自定义)!!!时间不宜过长
+        'timeout'	=>	'3600 * 12'
+    ],
     //百度编辑器自定义
     'toolbars'=>[[
         'source', //源代码
