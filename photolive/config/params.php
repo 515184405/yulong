8ed79af49658f0150ad82c2695c8922217e34773
fengjianji <515184405@qq.com>
2020-09-15 00:25:34 +0800

    a

--- photolive/config/params.php
+++ photolive/config/params.php
@@ -221,6 +221,8 @@ return [
         'accessKeySecret'=>'H51TnLzN1lBd6L4V7HyGmcJDBYAke8',
         'bucket' => 'sheyingpai',
         'endPoint' => 'oss-accelerate.aliyuncs.com',
+
+
     ],
     //oss 地址
     'oss_url' => '//sheyingpai.oss-cn-beijing.aliyuncs.com/',
