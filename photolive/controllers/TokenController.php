aa5f7e90729cb1a093201035d1730f5bf85b2bfe
fengjianji <515184405@qq.com>
2020-10-18 14:08:25 +0800

    a

--- photolive/controllers/TokenController.php
+++ photolive/controllers/TokenController.php
@@ -14,11 +14,9 @@ use common\models\UploadForm;
 use yii\helpers\Json;
 use yii\web\UploadedFile;
 
-header("Access-Control-Allow-Origin","*");
-header("Access-Control-Allow-Credentials", "true");
-header("Access-Control-Allow-Methods", "*");
-header("Access-Control-Allow-Headers", "Content-Type,Access-Token,token");
-header("Access-Control-Expose-Headers", "*");
+header("Access-Control-Allow-Methods", "OPTIONS, GET, POST");
+header("Access-Control-Allow-Headers", "x-requested-with");
+header("Access-Control-Max-Age", "86400");
 /**
  * API安全验证
  * @Author   NingWang
